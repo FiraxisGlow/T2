a2ab688361618542a51ca732cb322c9950ed9941
Anton Kiri <K3268@student.jamk.fi>
2016-01-19 11:03:31 +0200

    Tehtävä4

---
+++ Tehtävä4/Tehtävä4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+* Copyright (C) Anton Kiri
+*
+*
+* Olio-ohjelmointi
+*
+*
+* 11.02.16
+* Anton Kiri
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Tehtävä4/Tehtävä4/Vehicles.cs
@@ -0,0 +1,25 @@
+﻿/*
+* Copyright (C) Anton Kiri
+*
+*
+* Olio-ohjelmointi
+*
+*
+* 11.02.16
+* Anton Kiri
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä4
+{
+    class Vehicles
+    {
+    }
+}
