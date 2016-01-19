d2a43dd9253c4f7231e909ee6cb5b4768455067a
Anton Kiri <k3268@student.jamk.fi>
2016-01-19 19:57:58 +0200

    T5

---
+++ Tehtävä5/Tehtävä5/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+* Copyright (C) Anton Kiri
+*
+*
+* Olio-ohjelmointi
+*
+*
+* 11.02.16
+* Anton Kiri
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Opiskelija[] student = new Opiskelija[5];
+
+            student[0] = new Opiskelija();
+            student[0].Name = "Jarmo";
+            student[0].LastName = "Holopainen";
+            student[0].ID = "~9842";
+            student[0].Age = 22;
+
+            student[1] = new Opiskelija();
+            student[1].Name = "Pena";
+            student[1].LastName = "Taivainmäki";
+            student[1].ID = "K1025";
+            student[1].Age = 26;
+
+            student[2] = new Opiskelija();
+            student[2].Name = "Leena";
+            student[2].LastName = "Saananjärvi";
+            student[2].ID = "H1111";
+            student[2].Age = 19;
+
+            student[3] = new Opiskelija();
+            student[3].Name = "Pasi";
+            student[3].LastName = "Pihkasuo";
+            student[3].ID = "G5421";
+            student[3].Age = 24;
+
+            student[4] = new Opiskelija();
+            student[4].Name = "Hermanni";
+            student[4].LastName = "Kivipuu";
+            student[4].ID = "T1337";
+            student[4].Age = 20;
+
+            for (int o = 0; o < 5; o++)
+            {
+                student[o].PrintData();
+                student[o].AGE();
+            }
+
+            Console.WriteLine("Vuoden päästä: ");
+            for (int o = 0; o < 5; o++)
+            {
+                student[o].PrintData();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Tehtävä5/Tehtävä5/Student.cs
@@ -0,0 +1,44 @@
+﻿
+
+/*
+* Copyright (C) Anton Kiri
+*
+*
+* Olio-ohjelmointi
+*
+*
+* 11.02.16
+* Anton Kiri
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä5
+{
+    class Opiskelija
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+
+
+        public void PrintData()
+        {
+
+            Console.WriteLine("Student info: " + Name + " " + LastName + ", " + ID + ", " + Age);
+
+        }
+
+        public void AGE()
+        {
+            Age++;
+        }
+    }
+}
