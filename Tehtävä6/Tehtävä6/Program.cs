f5f39aa1673ea24aea0ee9e89f9d9e2dd732dbba
Anton Kiri <k3268@student.jamk.fi>
2016-01-19 21:24:03 +0200

    Harjoitus6

---
+++ Tehtävä6/Tehtävä6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monta autoa omistat?");
+            string rivi = Console.ReadLine();
+            int luku = int.Parse(rivi);
+
+            int max;
+            max = luku;
+
+            Car[] cars = new Car[max];
+
+            for (int i = 0; i < max; i++)
+            {
+                cars[i] = new Car();
+                Console.Write("Merkki " + (i + 1));
+                cars[i].Manufacturer = Console.ReadLine();
+
+                Console.Write("Auton " + (i + 1) + " Malli: ");
+                cars[i].Model = Console.ReadLine();
+
+                Console.Write("Auton vuosimalli" + (i + 1));
+                string rivi1 = Console.ReadLine();
+                cars[i].Ymodel = int.Parse(rivi1);
+
+             
+
+
+        
+            Console.WriteLine();
+        }
+
+            for (int y = 0; y < max; y++)
+            {
+                Console.WriteLine("Auton " + (y + 1) + " tiedot: ");
+                Console.WriteLine(cars[y].ToString());
+              
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Vuoden päästä: ");
+            for (int y = 0; y < max; y++)
+            {
+                Console.WriteLine(cars[y].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tehtävä6/Tehtävä6/Vehicles.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public int Ymodel { get; set; }
+        public string Manufacturer { get; set; }
+        
+     
+       
+        // method returns vehicle data
+        public override string ToString()
+        {
+            return "- Merkki: " + Manufacturer + " - Malli: " + Model + " - Vuosimalli: " + Ymodel;
+        }
+    }
+}
