a55bf24d28c955c63672824d997938f04840980e
Anton Kiri <K3268@student.jamk.fi>
2016-01-19 12:25:10 +0200

    Vehicles

--- Tehtävä4/Tehtävä4/Vehicles.cs
+++ Tehtävä4/Tehtävä4/Vehicles.cs
@@ -21,5 +21,24 @@ namespace Tehtävä4
 {
     class Vehicles
     {
+        //properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        //method
+        public void PrintData()
+        {
+            Console.WriteLine("Car name: " + Name);
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Current Tyres: " + Tyres);
+
+        }
+        public override string ToString()
+        {
+            return "Car Name: " +Name + "Car Speed" + Speed + "Number of tyres: " + Tyres; 
+        }
+
+
     }
 }
