4baedad2329a84dc45623e20f86a56d9cf3d7c54
Anton Kiri <K3268@student.jamk.fi>
2016-01-19 12:05:16 +0200

    vehicle data

---
+++ CarApp1/CarApp1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp1
+{
+    class Car
+    {
+
+        //fied member
+
+        //properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDice { get; set; }
+
+        //constructor
+
+        public Car()
+        {
+            FuzzyDice = true;
+        }
+        
+        //parametric constructor
+
+        public Car(string model)
+        {
+            Model = model;
+
+        }
+
+        //method displays car data
+        public void PrintData()
+        {
+            Console.WriteLine("Car Data: ");
+            Console.WriteLine("- model : " + Model);
+            Console.WriteLine("- color : " + Color);
+            Console.WriteLine("- engine : " + Engine);
+            Console.WriteLine("- speed : " + Speed);
+            Console.WriteLine("- dice : " + FuzzyDice);
+        }
+
+        //method gice more speed
+        public void Accelerate(int newSpeed)
+        {
+            Speed += newSpeed;
+        }
+
+
+    }
+}
